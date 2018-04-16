e296002942482d696fe413b290adaaf3c2acffb1
messi-yang <gracyme123@hotmail.com>
2018-04-16 19:59:39 +0800

    ui file auto-generate

--- tv_poweronoff_supervisory.py
+++ tv_poweronoff_supervisory.py
@@ -1,51 +1,46 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-'''
+"""
 author: Jeffrey
 date: 2018/4/10
-'''
-
+"""
 import sys
+import serial.tools.list_ports as list_ports
 from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtMultimedia import QCamera, QCameraImageCapture, QCameraInfo
-from PyQt5.QtMultimediaWidgets import QCameraViewfinder
 from mainwindow import Ui_Form
 
 
-
 class MainWindow(QWidget, Ui_Form):
-    '''
+    """
     Setup window
-    '''
+    """
 
     def __init__(self):
-        '''
-        Constructor
-        '''
+        """Constructor
+        """
+
         super(MainWindow, self).__init__()
         self.setupUi(self)
 
-        camera = QCamera(self)
-        viewfinder = QCameraViewfinder(self)
+        self.refresh_port_btn.clicked.connect(self.refresh_port)
+        self.open_port_btn.clicked.connect(self.open_port)
 
-        camera.setViewfinder(viewfinder)
-        imagecapture = QCameraImageCapture(camera)
+    def refresh_port(self):
+        self.port_lists_combox.clear()
+        com_list = list_ports.comports()
+        com_name_list = []
+        for com_item in com_list:
+            com_name_list.append(com_item[0])
+        self.port_lists_combox.addItems(sorted(com_name_list))
 
-        self.imageview.addWidget(viewfinder)
-        self.imagecapture.setScaledContents(True)
-
-        camera.start()
-
-        camerinfo = QCameraInfo(camera)
-        print(camerinfo.deviceName())
-
-        print(camera.status())
-
+    # TODO(Jeffrey): Will finish as soon as possible
+    def open_port(self):
+        pass
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    mainwindow = MainWindow()
-    mainwindow.show()
-    sys.exit(app.exec_())
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_()) 
