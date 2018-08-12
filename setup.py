dc61fbcde7fa93b2086a907a1c624eb402e1bc60
Jeffrey_Yang <gracyme123@hotmail.com>
2018-08-12 17:57:19 +0800

    partially finished setup.py

--- setup.py
+++ setup.py
@@ -5,3 +5,26 @@
 author: Jeffrey
 date: 2018/8/10
 """
+
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name = 'tvsupervisory',
+    version = '0.0.1',
+    author = 'Jeffrey Yang',
+    author_email = '',
+    description = 'Python tools for Tv power on and off supervisory',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/messi-yang/TvSupervisory',
+    packages = setuptools.find_packages(),
+    classifiers = {
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    }
+)
