f1f2ec53566df27abb8a6c623fbe719832fda272
messi-yang <gracyme123@hotmail.com>
2018-08-14 19:19:36 +0800

    unit test for ui

---
+++ tvsupervisory/test/test_mainwindow.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python  
+# -*- coding: utf-8 -*-
+
+"""
+author: Jeffrey
+date: 2018/8/14
+"""
+
+import sys
+
+from PyQt5 import QtWidgets
+
+from tvsupervisory.mainwindow import Ui_Form
+
+
+class Test_UI(QtWidgets.QWidget, Ui_Form):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
+def main():
+    """Module's main entrance, """
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = Test_UI()
+    main_window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
