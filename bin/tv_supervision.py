e205b611cb46aaca1c151689cf383b51af777794
Jeffrey_Yang <gracyme123@hotmail.com>
2018-08-17 23:51:38 +0800

    adjust import order

--- bin/tv_supervision.py
+++ bin/tv_supervision.py
@@ -6,16 +6,15 @@ author: Jeffrey
 date: 2018/5/14
 """
 
-
+# imports
 import os
 import sys
+
+from tvsupervision import main
+
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print(base_dir)
 sys.path.append(base_dir)
 
-from tvsupervision import main
-
 if __name__ == '__main__':
     main.main()
-
-
